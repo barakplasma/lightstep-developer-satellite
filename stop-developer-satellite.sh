f9584185aa91fab2410814d11ed19b78b7c9d16f
Michael Salaverry <barakplasma@gmail.com>
2021-12-13 12:08:02 +0200

    Update stop-developer-satellite.sh

--- stop-developer-satellite.sh
+++ stop-developer-satellite.sh
@@ -3,11 +3,11 @@
 NAME=lightstep_developer_satellite
 
 while true; do
-  ID=$(docker ps --all | grep ${NAME} | head -n 1 | cut -d ' ' -f 1 )
+  ID=$(nerdctl ps --all | grep ${NAME} | head -n 1 | cut -d ' ' -f 1 )
   if [ -n "$ID" ]; then
     echo "Removing docker container $ID"
-    docker kill "$ID" > /dev/null 2>&1 || true
-    docker rm "$ID" > /dev/null 2>&1 || true
+    nerdctl kill "$ID" > /dev/null 2>&1 || true
+    nerdctl rm "$ID" > /dev/null 2>&1 || true
   else
     break
   fi
